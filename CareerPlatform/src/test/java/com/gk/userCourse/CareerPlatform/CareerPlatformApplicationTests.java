2e40c8f9f30734ee21495bf64628bd0690bdd828
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-14 23:57:37 +0200

    Initial Bootstrap of Spring boot application

---
+++ CareerPlatform/src/test/java/com/gk/userCourse/CareerPlatform/CareerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.userCourse.CareerPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
