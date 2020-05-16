65face81de5a01b926ec8237f211cb8956a9f115
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-16 16:24:53 +0200

    Adding the service layer to the rest controller

--- API/src/main/java/com/gk/userCourse/CareerPlatform/Rest/UserRest.java
+++ API/src/main/java/com/gk/userCourse/CareerPlatform/Rest/UserRest.java
@@ -2,7 +2,6 @@ package com.gk.userCourse.CareerPlatform.Rest;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,48 +11,47 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gk.userCourse.CareerPlatform.Dao.UserDao;
 import com.gk.userCourse.CareerPlatform.Entity.User;
 import com.gk.userCourse.CareerPlatform.Entity.UserDetails;
 import com.gk.userCourse.CareerPlatform.ErrorHandling.UserNotFoundExecption;
+import com.gk.userCourse.CareerPlatform.Service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserRest {
 
-	private UserDao userDao;
+	private UserService userService;
 
 	@Autowired
-	public UserRest(UserDao userDao) {
-		this.userDao = userDao;
+	public UserRest(UserService theUserService) {
+		this.userService = theUserService;
 	}
 
 	@GetMapping("/users")
 	public List<User> getUsers() {
-		return userDao.findAll();
+		return userService.findAll();
 	}
 
 	@GetMapping("/users/{userId}")
 	public User getSingleUser(@PathVariable int userId) {
-		if (userId < 0 || userId > userDao.findAll().size()) {
+		if (userId < 0 || userId > userService.findAll().size()) {
 			throw new UserNotFoundExecption("User Id Not Found - " + userId);
 		}
-
-		return userDao.findById(userId);
+		return userService.findbyId(userId);
 	}
 
 	@PostMapping("/users")
 	public User saveUsers(@RequestBody User theUser) {
 		theUser.setId(0);
-		userDao.save(theUser);
+		userService.save(theUser);
 		return theUser;
 	}
 
 	@PostMapping("/usersDetails/{userId}")
 	public User saveUsers(@PathVariable int userId, @RequestBody UserDetails theUserDetails) {
-		User tempUser = userDao.findById(userId);
+		User tempUser = userService.findbyId(userId);
 		tempUser.setUserDetails(theUserDetails);
-		userDao.save(tempUser);
+		userService.save(tempUser);
 		return tempUser;
 	}
 
