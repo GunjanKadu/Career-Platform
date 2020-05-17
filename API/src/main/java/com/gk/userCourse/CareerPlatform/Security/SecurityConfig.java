15e29dca373a5842b218367443f7dedd3a8df59f
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-17 12:43:30 +0200

    Updating the security config to validate JWT

--- API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
+++ API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
@@ -9,7 +9,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.gk.userCourse.CareerPlatform.Service.CustomUserDetailService;
+import com.gk.userCourse.CareerPlatform.Util.JwtFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -17,6 +21,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private CustomUserDetailService customUserDetailsService;
+
+	@Autowired
+	private JwtFilter jwtFilter;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,6 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/api/login").permitAll().anyRequest().authenticated();
+		http.csrf().disable().authorizeRequests().antMatchers("/api/login","/api/signin").permitAll().anyRequest().authenticated()
+				.and().exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		;
 	}
 }
