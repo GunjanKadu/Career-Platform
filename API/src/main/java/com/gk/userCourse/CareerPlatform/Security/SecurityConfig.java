01f3b12a320aceae7dcf15a0d1084de7a018fdfd
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-17 02:43:46 +0200

    Adding JWT

--- API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
+++ API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.gk.userCourse.CareerPlatform.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import com.gk.userCourse.CareerPlatform.Service.CustomUserDetailService;
@@ -19,4 +23,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(customUserDetailsService);
 	}
 
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/api/login").permitAll().anyRequest().authenticated();
+	}
 }
