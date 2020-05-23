3cc014e8a8739f487c74b97d8675f10eea1eca7d
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-23 14:18:23 +0200

    Add cors support

--- API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
+++ API/src/main/java/com/gk/userCourse/CareerPlatform/Security/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
@@ -48,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -55,10 +61,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.addAllowedOrigin("*");
+                config.setAllowCredentials(true);
+                return config;
+            }
+        })
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/login", "/api/signin").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/featuredCourses").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/featuredCourses").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
