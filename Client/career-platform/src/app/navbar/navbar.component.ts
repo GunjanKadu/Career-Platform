2b1a28d4d973dbb73eea2f81c1fe7cfe1a1e3d57
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-23 18:49:39 +0200

    Submitting the form and recieving the jwt token

--- Client/career-platform/src/app/navbar/navbar.component.ts
+++ Client/career-platform/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "./authenticationService";
 
 @Component({
   selector: "app-navbar",
@@ -8,12 +10,40 @@ import { NgForm } from "@angular/forms";
 })
 export class NavbarComponent implements OnInit {
   @ViewChild("formData", { static: true }) signUpForm: NgForm;
+  @ViewChild("closeModal", { static: true }) closeModal: ElementRef;
 
-  constructor() {}
+  public userName: string;
+  public password: string;
+  public error: string = null;
+  public isLoading: boolean;
+
+  constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit() {}
 
   onSubmitLogin() {
-    console.log(this.signUpForm);
+    this.userName = this.signUpForm.value.username;
+    this.password = this.signUpForm.value.password;
+    this.isLoading = true;
+
+    setTimeout(
+      () =>
+        this.authenticationService
+          .loginService(this.userName, this.password)
+          .subscribe(
+            (response: any) => {
+              this.signUpForm.reset();
+              this.isLoading = false;
+              this.closeModal.nativeElement.click();
+              console.log(response);
+            },
+            (error: any) => {
+              this.isLoading = false;
+              this.error = "Invalid UserName Or Password";
+              setTimeout(() => (this.error = null), 2500);
+            }
+          ),
+      1000
+    );
   }
 }
