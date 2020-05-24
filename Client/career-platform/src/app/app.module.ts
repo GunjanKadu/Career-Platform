b59f53232ceff86f6e321beb445e6c0274c49685
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-24 14:13:46 +0200

    Adding redux to the app module

--- Client/career-platform/src/app/app.module.ts
+++ Client/career-platform/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment.prod";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SpinnerComponent } from "./navbar/spinner/spinner.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { HomeComponent } from "./home/home.component";
+import { AuthenticationReducer } from "./redux/reducers/Authentication";
 
 @NgModule({
   declarations: [
@@ -16,7 +20,17 @@ import { HomeComponent } from "./home/home.component";
     SpinnerComponent,
     HomeComponent,
   ],
-  imports: [FormsModule, BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ authentication: AuthenticationReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
