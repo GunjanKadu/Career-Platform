78e0745bfa1ccc999a657d119f0585fbccb25b81
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-24 17:29:38 +0200

    End Point for Finding users by username

--- API/src/main/java/com/gk/userCourse/CareerPlatform/Rest/UserRest.java
+++ API/src/main/java/com/gk/userCourse/CareerPlatform/Rest/UserRest.java
@@ -1,58 +1,62 @@
 package com.gk.userCourse.CareerPlatform.Rest;
 
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.gk.userCourse.CareerPlatform.Entity.User;
 import com.gk.userCourse.CareerPlatform.Entity.UserDetails;
 import com.gk.userCourse.CareerPlatform.ErrorHandling.UserNotFoundExecption;
 import com.gk.userCourse.CareerPlatform.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class UserRest {
 
-	private UserService userService;
+    private UserService userService;
 
-	@Autowired
-	public UserRest(UserService theUserService) {
-		this.userService = theUserService;
-	}
+    @Autowired
+    public UserRest(UserService theUserService) {
+        this.userService = theUserService;
+    }
 
-	@GetMapping("/users")
-	public List<User> getUsers() {
-		return userService.findAll();
-	}
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.findAll();
+    }
 
-	@GetMapping("/users/{userId}")
-	public User getSingleUser(@PathVariable int userId) {
-		if (userId < 0 || userId > userService.findAll().size()) {
-			throw new UserNotFoundExecption("User Id Not Found - " + userId);
-		}
-		return userService.findbyId(userId);
-	}
+    @GetMapping("/users/{userId}")
+    public User getSingleUser(@PathVariable int userId) {
+        if (userId < 0 || userId > userService.findAll().size()) {
+            throw new UserNotFoundExecption("User Id Not Found - " + userId);
+        }
+        return userService.findbyId(userId);
+    }
 
-	@PostMapping("/users")
-	public User saveUsers(@RequestBody User theUser) {
-		theUser.setId(0);
-		userService.save(theUser);
-		return theUser;
-	}
+    @PostMapping("/users/getByUserName")
+    public User getSingleUser(@RequestBody String userName) {
+        if (userName.length() < 0) {
+            throw new UserNotFoundExecption("User Name Not Found - " + userName);
+        }
+        List<User> allUsers = userService.findAll();
+        List<User> foundUser = allUsers.stream().filter(User -> User.getEmail().equals(userName)).collect(Collectors.toList());
+        return foundUser.get(0);
+    }
 
-	@PostMapping("/usersDetails/{userId}")
-	public User saveUsers(@PathVariable int userId, @RequestBody UserDetails theUserDetails) {
-		User tempUser = userService.findbyId(userId);
-		tempUser.setUserDetails(theUserDetails);
-		userService.save(tempUser);
-		return tempUser;
-	}
+    @PostMapping("/users")
+    public User saveUsers(@RequestBody User theUser) {
+        theUser.setId(0);
+        userService.save(theUser);
+        return theUser;
+    }
+
+    @PostMapping("/usersDetails/{userId}")
+    public User saveUsers(@PathVariable int userId, @RequestBody UserDetails theUserDetails) {
+        User tempUser = userService.findbyId(userId);
+        tempUser.setUserDetails(theUserDetails);
+        userService.save(tempUser);
+        return tempUser;
+    }
 
 }
