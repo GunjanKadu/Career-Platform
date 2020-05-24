83bb8de11ae4ea1d8bd0e6e32bfa5b7dbc6f4e4f
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-24 12:28:14 +0200

    AUthentication service for signin users

--- Client/career-platform/src/app/navbar/authenticationService.ts
+++ Client/career-platform/src/app/navbar/authenticationService.ts
@@ -5,17 +5,41 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  loginService(username: string, password: string) {
+  loginService(user: User) {
     const headers = new HttpHeaders({
       "Content-Type": "application/json",
     });
     return this.http.post(
       "http://localhost:8080/api/login",
       {
-        userName: username,
-        passwordString: password,
+        userName: user.username,
+        passwordString: user.password,
       },
       { headers, responseType: "text" }
     );
   }
+
+  signupService(user: User) {
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+    });
+    return this.http.post(
+      "http://localhost:8080/api/signin",
+      {
+        password: user.password,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+      { headers }
+    );
+  }
+}
+
+export interface User {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  username?: string;
 }
