64f5592f334f1806fd9afdcdfcee5ea8ebcd3c76
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-06-02 20:33:39 +0200

    refactoring other components

--- Client/career-platform/src/app/app.routing.module.ts
+++ Client/career-platform/src/app/app.routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { CoursesComponent } from "./courses/courses.component";
+import { CoursedescComponent } from "./courses/coursedesc/coursedesc.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "courses", component: CoursesComponent },
+  { path: "course/:courseName/:id", component: CoursedescComponent },
 ];
 
 @NgModule({
