aaf92d62bf529dabef198620e48b1a6e088cd29e
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-06-05 19:41:24 +0200

    Creating the forms and design for creating a course

--- Client/career-platform/src/app/createcourse/createcourse.component.ts
+++ Client/career-platform/src/app/createcourse/createcourse.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-createcourse',
-  templateUrl: './createcourse.component.html',
-  styleUrls: ['./createcourse.component.css']
+  selector: "app-createcourse",
+  templateUrl: "./createcourse.component.html",
+  styleUrls: ["./createcourse.component.css"],
 })
 export class CreatecourseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
+  public selectedValue: string = "info";
+  public today: Date;
+  public imagePreview: string = null;
   ngOnInit() {
+    this.today = new Date();
   }
 
+  whichFormToShow(value: string) {
+    this.selectedValue = value;
+  }
+  onImageSelect(event: any) {
+    console.log();
+    this.imagePreview = event.srcElement.value;
+  }
 }
