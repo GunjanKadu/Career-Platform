47ce539d8626c41a7b412a95735bda6e9a1f41b5
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-06-09 16:31:41 +0200

    Bundling in electron js

--- src/app/courses/courseservice.service.spec.ts
+++ src/app/courses/courseservice.service.spec.ts
@@ -1,16 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { CourseserviceService } from './courseservice.service';
+import { CourseserviceService } from "./courseservice.service";
 
-describe('CourseserviceService', () => {
+describe("CourseserviceService", () => {
   let service: CourseserviceService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CourseserviceService);
-  });
-
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
